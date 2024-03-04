e23e457dcf660b08fac535ccc716bab91b21c090
Kolbi03 <lengyel.milan.hu@gmail.com>
2024-03-04 10:04:24 +0100

    Endpoints done

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,5 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
-import { BooksService } from './books.service';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { BooksService, BookInput, BookDTO } from './books.service';
 
 @Controller('books')
 export class BooksController {
@@ -8,5 +17,29 @@ export class BooksController {
   @Get('')
   getAllBooks() {
     return this.booksService.getAllBooks();
+  }
+
+  @Get('/:id')
+  getBookById(@Param('id') id: string) {
+    const book = this.booksService.getBookById(id);
+    if (!book) {
+      throw new NotFoundException();
+    }
+    return book;
+  }
+
+  @Post('/newBook')
+  createBook(@Body() book: BookInput) {
+    this.booksService.createBook(book);
+  }
+
+  @Delete('/delete/:id')
+  deleteBook(@Param('id') id: string) {
+    this.booksService.deleteBook(id);
+  }
+
+  @Put('/update/:id')
+  updateBook(@Param('id') id: string, @Body() body: BookDTO) {
+    this.booksService.updateBook(id, body);
   }
 }
--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,17 +1,55 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Book } from './books.model';
+
+export class BookInput {
+  title: string;
+  author: string;
+  publishYear: string;
+}
+
+export type BookDTO = Omit<Book, 'id'>;
 
 @Injectable()
 export class BooksService {
   private readonly books: Book[] = [
     {
+      id: '0',
       title: 'asd',
       author: 'asd',
-      publishYear: 2000,
+      publishYear: '2000',
     },
   ];
 
   getAllBooks() {
     return this.books;
+  }
+
+  getBookById(id: string) {
+    return this.books.find((book) => book.id === id);
+  }
+
+  public createBook(bookInput: BookInput) {
+    const book: Book = {
+      id: Math.random().toString(),
+      ...bookInput,
+    };
+    this.books.push(book);
+  }
+
+  deleteBook(id: string) {
+    const index = this.books.findIndex((book) => book.id === id);
+    if (index === -1) {
+      return;
+    }
+    this.books.splice(index, 1);
+  }
+
+  updateBook(id: string, input: BookDTO) {
+    const book = this.getBookById(id);
+    if (!book) {
+      throw new NotFoundException();
+    }
+    Object.assign(book, input);
+    return book;
   }
 }
